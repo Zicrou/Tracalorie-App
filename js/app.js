531b59759c14963ed8f2b9d23389850e78023b6f
Zicrou <aazseck@gmail..com>
2023-11-21 18:21:01 +0000

    App Class, New Meal and Workout

--- js/app.js
+++ js/app.js
@@ -107,16 +107,60 @@ class Workout{
     }
 }
 
-const tracker = new CalorieTracker();
+class App{
+    constructor() {
+        this._tracker = new CalorieTracker();
+        document.getElementById('meal-form').addEventListener('submit', this._newMeal.bind(this))
+        document.getElementById('workout-form').addEventListener('submit', this._newWorkout.bind(this))
+    }
 
-const breakfast = new Meal('Breakfast', 400);
-tracker.addMeal(breakfast);
-const lunch = new Meal('Lunch', 350);
-tracker.addMeal(lunch);
+    _newMeal(e) {
+        e.preventDefault();
 
-const run = new Workout('Morning Run', 320);
-tracker.addWorkout(run);
+        const name = document.getElementById('meal-name');
+        const calories = document.getElementById('meal-calories');
 
-console.log(tracker._meals);
-console.log(tracker._workouts);
-console.log(tracker._totalCalories);
+        // Validate inputs
+        if (name.value === '' || calories.value === '') {
+            alert('Please fill in all fields');
+            return;
+        }
+
+        const meal = new Meal(name.value, +calories.value);
+        this._tracker.addMeal(meal);
+
+        name.value = '';
+        calories.value = '';
+
+        const collapseMeal = document.getElementById('collapse-meal');
+        const bsCollapse = new bootstrap.Collapse(collapseMeal, {
+            toggle: true
+        });
+    }
+
+    _newWorkout(e) {
+        e.preventDefault();
+
+        const name = document.getElementById('workout-name');
+        const calories = document.getElementById('workout-calories');
+
+        // Validate inputs
+        if (name.value === '' || calories.value === '') {
+            alert('Please fill in all fields');
+            return;
+        }
+
+        const workout = new Workout(name.value, +calories.value);
+        this._tracker.addWorkout(workout);
+
+        name.value = '';
+        calories.value = '';
+
+        const collapseWorkout = document.getElementById('collapse-workout');
+        const bsCollapse = new bootstrap.Collapse(collapseWorkout, {
+            toggle: true
+        })
+    }
+}
+
+const app = new App();
