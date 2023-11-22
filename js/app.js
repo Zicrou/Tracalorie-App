003561a29fa4e5ec22155df40165f9de46fad38b
Zicrou <aazseck@gmail..com>
2023-11-22 15:08:00 +0000

    Set Calorie Limit

--- js/app.js
+++ js/app.js
@@ -57,6 +57,12 @@ class CalorieTracker{
         this._render();
     }
 
+    setLimit(calorieLimit){
+        this._calorieLimit = calorieLimit;
+        this._displayCaloriesLimit();
+        this._render();
+    }
+
     // Private Methods //
     
     _displayCaloriesTotal(){
@@ -203,6 +209,8 @@ class App{
         document.getElementById('filter-workouts').addEventListener('keyup', this._filterItems.bind(this, 'workout'));
 
         document.getElementById('reset').addEventListener('click', this._reset.bind(this));
+        
+        document.getElementById('limit-form').addEventListener('submit', this._setLimit.bind(this));
     }
 
     _newItem(type, e) {
@@ -211,7 +219,7 @@ class App{
         const name = document.getElementById(`${type}-name`);
         const calories = document.getElementById(`${type}-calories`);
 
-        // Validate inputs
+        // Validate inputs 
         if (name.value === '' || calories.value === '') {
             alert('Please fill in all fields');
             return;
@@ -267,6 +275,24 @@ class App{
         document.getElementById('workout-items').innerHTML = '';
         document.getElementById('filter-meals').value = '';
         document.getElementById('filter-workouts').value = '';
+    }
+
+    _setLimit(e){
+        e.preventDefault();
+
+        const limit = document.getElementById('limit');
+
+        if (limit.value === '') {
+            alert('Please add a limit');
+            return;
+        }
+
+        this._tracker.setLimit(+limit.value);
+        limit.value = '';
+
+        const modalEl = document.getElementById('limit-modal');
+        const modal = bootstrap.Modal.getInstance(modalEl);
+        modal.hide();
     }
 }
 
